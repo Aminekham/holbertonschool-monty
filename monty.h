f34daf4e0ae436eeb3f40c9e4d287bdc83e53bc5
Aminekham <lkham0508@gmail.com>
2022-12-23 15:37:31 +0100

    monty header

--- monty.h
+++ monty.h
@@ -39,6 +39,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void execute(__attribute__((unused)) char *line, stack_t **stack, unsigned int line_number);
+int is_digit(char *value);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
