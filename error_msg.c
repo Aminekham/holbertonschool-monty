bcda9cbd5a64352febe60fd5eda773ab2a2d41d0
weedii <abidiw293@gmail.com>
2022-12-25 00:06:44 +0100

    fix err msg

--- error_msg.c
+++ error_msg.c
@@ -12,28 +12,11 @@ void error_boss(char *opcode, unsigned int line_number)
 		push_not_found(opcode, line_number);
 		return;
 	}
-	else if (strcmp(opcode, "pint") == 0)
+	else
 	{
 		pint_error(opcode, line_number);
 		return;
 	}
-	else if (strcmp(opcode, "pop") == 0)
-	{
-		pop_error(opcode, line_number);
-		return;
-	}
-	else if (strcmp(opcode, "swap") == 0)
-	{
-		swap_error(opcode, line_number);
-		return;
-	}
-	else if (strcmp(opcode, "add") == 0)
-	{
-		add_error(opcode, line_number);
-		return;
-	}
-	else
-		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 }
 
 /**
@@ -42,36 +25,6 @@ void error_boss(char *opcode, unsigned int line_number)
  * @line_number: line number
  */
 void pint_error(char *opcode, unsigned int line_number)
-{
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-}
-
-/**
- * pop_error - function that print error msg for pop
- * @opcode: the opcode
- * @line_number: line number
- */
-void pop_error(char *opcode, unsigned int line_number)
-{
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-}
-
-/**
- * swap_error - function that print error msg for swap
- * @opcode: the opcode
- * @line_number: line number
- */
-void swap_error(char *opcode, unsigned int line_number)
-{
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-}
-
-/**
- * add_error - function that print error msg for add
- * @opcode: the opcode
- * @line_number: line number
- */
-void add_error(char *opcode, unsigned int line_number)
 {
 	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 }
